0f79003863717c60dd9c1f5b20f0ecdeb3c94695
Erdal ALTIN <erdalaltin@gmail.com>
2022-11-03 13:44:31 +0300

    Update index.php

--- fat-free/webapp/public/index.php
+++ fat-free/webapp/public/index.php
@@ -5,7 +5,7 @@ require ( __DIR__ . '/../vendor/autoload.php' );
 $f3 = \Base::instance();
 $f3->route('GET /',
     function() {
-        echo 'Hello, world!';
+        echo '<html><body><br/><br/><center><p style=\"font-size:55px\">Hello, world!</p></center></body></html>';
     }
 );
 $f3->run();
